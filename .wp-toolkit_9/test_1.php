2fe0b246b03f5447b54eaba0c8aedb6f9cb9ec43
Gitium <gitium@presslabs.com>
2026-04-08 03:54:25 -0400

    modified file `test_1.php`

---
+++ .wp-toolkit_9/test_1.php
@@ -0,0 +1,14 @@
+
+<?php
+
+$url = 'https://abc.com/';
+$useragent = 'Opera/9.80 (J2ME/MIDP; Opera Mini/4.2.14912/870; U; id) Presto/2.4.15';
+$ch = curl_init('');
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_USERAGENT, $useragent); // set user agent
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+echo $output = curl_exec($ch);
+curl_close($ch);
+echo $contents;
+$url="https://abc.com/lms"
