f2bde15b4f2a73c0873e1a77e925762307b9bc80
Gitium <gitium@presslabs.com>
2026-04-28 04:04:40 -0400

    updated plugin `Thrive Apprentice` version 10.8.10.2

---
+++ .wp-toolkit_l/wp-content/plugins/thrive-apprentice/thrive-dashboard/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'thrive-themes/thrive-dashboard',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd1518357fd20da63dc02f61d6f7eca6fbdcd7739',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'thrive-themes/thrive-dashboard' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd1518357fd20da63dc02f61d6f7eca6fbdcd7739',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
