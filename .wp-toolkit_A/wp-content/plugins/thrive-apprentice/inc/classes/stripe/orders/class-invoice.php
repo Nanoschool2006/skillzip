dd82a8f29e4a5acbb403cfb30cf57700947880d6
Gitium <gitium@presslabs.com>
2026-05-03 03:29:48 -0400

    updated plugin `Thrive Apprentice` version 10.9

---
+++ .wp-toolkit_A/wp-content/plugins/thrive-apprentice/inc/classes/stripe/orders/class-invoice.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Thrive Themes - https://thrivethemes.com
+ *
+ * @package thrive-apprentice
+ */
+
+namespace TVA\Stripe\Orders;
+
+use TVA\Stripe\Helper;
+use TVA\Stripe\Hooks;
+use TVA\Stripe\Request;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Silence is golden!
+}
+
+class Invoice extends Checkout {
+	/**
+	 * Add current order data to the order object
+	 *
+	 * @return void
+	 */
+	public function process_data() {
+		$this->order->set_payment_id( $this->data->id );
+		$this->order->set_currency( $this->data->currency );
+		$this->order->set_buyer_email( $this->data->customer_email );
+		$this->order->set_price( $this->data->total / 100 );
+		$this->order->set_price_gross( $this->data->total / 100 );
+
+		list( $first_name, $last_name ) = Helper::get_names( $this->data->customer_email );
+		$user = $this->get_user( $this->data->customer_email, $first_name, $last_name );
+
+		if ( $user ) {
+			$this->order->set_user_id( $user->ID );
+			$customer = $this->data->customer;
+			if ( ! $customer ) {
+				$customer = Request::get_customer_id( $this->data->customer_email );
+			}
+			update_user_meta( $user->ID, Hooks::CUSTOMER_META_ID, $customer );
+		}
+
+		if ( ! empty( $this->data->line_items ) ) {
+			$this->process_line_items( $this->data->line_items );
+		}
+	}
+}
