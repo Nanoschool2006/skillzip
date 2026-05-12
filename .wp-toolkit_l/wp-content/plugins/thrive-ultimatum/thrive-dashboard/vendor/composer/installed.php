0c359ea7d1d531d4fa7b78f5e73a8e31dc76d0c8
Gitium <gitium@presslabs.com>
2026-05-12 03:55:39 -0400

    updated plugin `Thrive Ultimatum` version 10.9

--- .wp-toolkit_l/wp-content/plugins/thrive-ultimatum/thrive-dashboard/vendor/composer/installed.php
+++ .wp-toolkit_l/wp-content/plugins/thrive-ultimatum/thrive-dashboard/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'thrive-themes/thrive-dashboard',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'd1518357fd20da63dc02f61d6f7eca6fbdcd7739',
+        'reference' => '7e798b8a405135942f081482d20f3c3ed68d2612',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'thrive-themes/thrive-dashboard' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'd1518357fd20da63dc02f61d6f7eca6fbdcd7739',
+            'reference' => '7e798b8a405135942f081482d20f3c3ed68d2612',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
