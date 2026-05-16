9fcbc93645273a64c9527b103c31f0d23ffea332
Gitium <gitium@presslabs.com>
2026-05-16 04:24:55 -0400

    updated plugin `Thrive Product Manager` version 10.9.1

--- .wp-toolkit_d/wp-content/plugins/thrive-product-manager/thrive-dashboard/vendor/composer/installed.php
+++ .wp-toolkit_d/wp-content/plugins/thrive-product-manager/thrive-dashboard/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'thrive-themes/thrive-dashboard',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '7e798b8a405135942f081482d20f3c3ed68d2612',
+        'reference' => '0d37073014761b7c39d667f9b7ff330b4d6947f8',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'thrive-themes/thrive-dashboard' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '7e798b8a405135942f081482d20f3c3ed68d2612',
+            'reference' => '0d37073014761b7c39d667f9b7ff330b4d6947f8',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
