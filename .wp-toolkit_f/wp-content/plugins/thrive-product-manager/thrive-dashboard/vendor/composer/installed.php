c5e8b27d836587bd554b3dd360bbea0af29c5e10
Gitium <gitium@presslabs.com>
2026-05-18 04:37:30 -0400

    updated plugin `Thrive Product Manager` version 10.9.1

--- .wp-toolkit_f/wp-content/plugins/thrive-product-manager/thrive-dashboard/vendor/composer/installed.php
+++ .wp-toolkit_f/wp-content/plugins/thrive-product-manager/thrive-dashboard/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'thrive-themes/thrive-dashboard',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'd1518357fd20da63dc02f61d6f7eca6fbdcd7739',
+        'reference' => '0d37073014761b7c39d667f9b7ff330b4d6947f8',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'thrive-themes/thrive-dashboard' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'd1518357fd20da63dc02f61d6f7eca6fbdcd7739',
+            'reference' => '0d37073014761b7c39d667f9b7ff330b4d6947f8',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
--- .wp-toolkit_f/wp-content/plugins/thrive-product-manager/thrive-dashboard/version.php
+++ .wp-toolkit_f/wp-content/plugins/thrive-product-manager/thrive-dashboard/version.php
@@ -35,4 +35,4 @@ if ( ! function_exists( 'tve_dash_load' ) ) {
 	}
 }
 
-return '10.8.10.2';
+return '10.9.1';
