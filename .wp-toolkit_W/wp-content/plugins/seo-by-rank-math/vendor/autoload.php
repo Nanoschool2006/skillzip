ea65f0526fe58037956b5bb47a399fd77be89aa8
Gitium <gitium@presslabs.com>
2026-05-24 03:43:56 -0400

    updated plugin `Rank Math SEO` version 1.0.270

--- .wp-toolkit_W/wp-content/plugins/seo-by-rank-math/vendor/autoload.php
+++ .wp-toolkit_W/wp-content/plugins/seo-by-rank-math/vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite95549a5e0142e077cfe2d40b9d57027::getLoader();
+return ComposerAutoloaderInit53ce9afc4fde41331e9e540f4a08054c::getLoader();
