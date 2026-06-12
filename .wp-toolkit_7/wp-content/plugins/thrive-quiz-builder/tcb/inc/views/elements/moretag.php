9e246401461f1bcedf549835f0b248b7cd8697a7
Amit Kumar Rai <amit.rai@celnet.in>
2026-06-12 02:28:56 -0400

    deleted file `wordfence.php`

--- .wp-toolkit_7/wp-content/plugins/thrive-quiz-builder/tcb/inc/views/elements/moretag.php
+++
@@ -1,1 +0,0 @@
-<p class="tve_more_tag"><span class="tve_no_edit"><?php echo esc_html__( 'More...', 'thrive-cb' ) ?></span></p>
--- .wp-toolkit_7/wp-content/plugins/thrive-ultimatum/tcb/inc/backbone/post-list/post-lp-button-tooltip.php
+++
@@ -1,5 +0,0 @@
-<div class="control-grid full-width pb-0">
-	<span>
-		<?php echo esc_html__( 'Landing Page Typography is not available when inheriting active theme typography is enabled.', 'thrive-cb' ); ?>
-	</span>
-</div>
