eb07c7057461195ba7e095c5ea066ff1e27dfdaa
Amit Kumar Rai <amit.rai@celnet.in>
2026-06-12 02:27:52 -0400

    deleted file `version.php`

--- .wp-toolkit_3/wp-content/plugins/thrive-product-manager/thrive-dashboard/inc/app-notification/assets/js/collections/Notifications.js
+++
@@ -1,6 +0,0 @@
-var NotificationModel = require( './../models/Notification' );
-
-module.exports = Backbone.Collection.extend({
-	model: NotificationModel,
-});
-
--- .wp-toolkit_3/wp-content/plugins/thrive-quiz-builder/tcb-bridge/editor-layouts/elements/social-share-badge.php
+++
@@ -1,5 +0,0 @@
-<div class="tqb-social-share-badge-container tcb-no-url thrv_wrapper thrv_social tcb-elem-placeholder tcb-no-clone">
-		<span class="tcb-inline-placeholder-action">
-		<?php echo esc_html__( 'Insert Social Share Badge', 'thrive-quiz-builder' ); ?>
-	</span>
-</div>
