db1fa00346ebad8d8750cecae238746ebbc2b7a2
Amit Kumar Rai <amit.rai@celnet.in>
2026-09-08 01:17:43 -0400

    deleted file `wordfence.php`

--- .wp-toolkit_k/wp-content/plugins/thrive-ultimatum/admin/views/dashboard.php
+++
@@ -1,4 +0,0 @@
-<div class="tvu-header"></div>
-<div class="tvu-breadcrumbs-wrapper" id="tvu-breadcrumbs-wrapper"></div>
-<?php echo tvd_get_individual_plugin_license_message( new TU_Product(), true ); ?>
-<div id="tvu-admin-wrapper"></div>
